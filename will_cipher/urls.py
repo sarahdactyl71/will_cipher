79d4e05b848e2f03f2a0f437c70689a75caa7373
Sarah Kirk <kirkse710@gmail.com>
2018-10-25 15:46:24 -0700

    starts fleshing out beginning app structure

--- will_cipher/urls.py
+++ will_cipher/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
 ]

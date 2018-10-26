97d297a5d68379be7d1ab60549597c9c790103f2
Sarah Kirk <kirkse710@gmail.com>
2018-10-26 15:41:30 -0700

    adds commented out paths to views.py

--- ciphers/views.py
+++ ciphers/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the ciphers index.")
+
+# def show(request, cipher_id):
+#     return HttpResponse()
+#
+# def create(request, cipher_id):
+#     return HttpResponse()
+#
+# def destroy(request, cipher_id):
+#     return HttpResponse()

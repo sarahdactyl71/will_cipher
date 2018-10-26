2604779bf9c65fe31f8d546f05d488763fa9bd57
Sarah Kirk <kirkse710@gmail.com>
2018-10-26 09:44:38 -0700

    adds additional models to models.py

--- ciphers/models.py
+++ ciphers/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 class Caesar(models.Model):
     caesar_text = models.CharField(max_length=500)
+
+class Atbash(models.Model):
+    atbash_test = model.CharField(max_length=500)
+
+class Alphanumeric(models.Model):
+    alphanumeric_text = model.CharField(max_length=500)
+
+class Viginere(model.Model):
+    viginere_text = model.CharField(max_length=500)
+    

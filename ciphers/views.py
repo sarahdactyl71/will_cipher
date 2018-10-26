628b58f81815b5c25a2815cc0c8ba839e5003e58
Sarah Kirk <kirkse710@gmail.com>
2018-10-26 15:48:44 -0700

    updates show method in views.py, adds template file structure

--- ciphers/views.py
+++ ciphers/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the ciphers index.")
 
-# def show(request, cipher_id):
-#     return HttpResponse()
+def show(request, cipher_id):
+    cipher = get_object_or404(Question, pk=cipher_id)
+    return render(request, 'ciphers/show.html', {'cipher': cipher})
 #
 # def create(request, cipher_id):
 #     return HttpResponse()

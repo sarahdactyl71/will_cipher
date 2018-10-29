1af8a88344e6ce8a915872cada4bc36dd7ac5683
Sarah Kirk <kirkse710@gmail.com>
2018-10-29 12:05:59 -0700

    adds additional information to caesar show page, adds some beginning methods to Ceasar class

--- ciphers/models.py
+++ ciphers/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Caesar(models.Model):
     caesar_text = models.CharField(max_length=500)
 
+    def encode():
+
+    def decode():
+        
+
 class Atbash(models.Model):
     atbash_test = models.CharField(max_length=500)
 

1cc815a0de5f66440a80586210a8a1337740a5b3
Sarah Kirk <kirkse710@gmail.com>
2018-10-29 19:26:17 -0700

    adds two more tests to tests.py

--- ciphers/tests.py
+++ ciphers/tests.py
@@ -7,11 +7,17 @@ from .models import Caesar, Atbash, Alphanumeric, Viginere
 
 class CaesarTestCase(TestCase):
 
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
+    def test_can_decode_theme_song_cipher(self):
         offset = 3
-        secret = 'VWDQ LV QRW ZKDW KH VHHPV'
-        self.assertIs(future_question.was_published_recently(), False)
+        caesar = Caesar.objects.create(caesar_text = 'VWDQ LV QRW ZKDW KH VHHPV')
+        self.assertEqual(caesar.decode(), 'STAN IS NOT WHAT HE SEEMS')
+
+    def test_can_decode_tourist_trapped(self):
+        offset = 3
+        caesar = Caesar.objects.create(caesar_text = 'ZHOFRPH WR JUDYLWB IDOOV.')
+        self.assertEqual(caesar.decode(), 'WELCOME TO GRAVITY FALLS.')
+
+    def test_can_decode_the_legend_of_gobbleonker(self):
+        offset = 3
+        caesar = Caesar.objects.create(caesar_text = 'QHAW ZHHN: UHWXUQ WR EXWW LVODQG.')
+        self.assertEqual(caesar.decode(), 'NEXT WEEK: RETURN TO BUTT ISLAND.')

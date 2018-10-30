a30c31b0bdb2a310ceacbbcfa2a359167a18e2e2
Sarah Kirk <kirkse710@gmail.com>
2018-10-29 19:19:18 -0700

    begins writng tests for Caesar cipher

--- ciphers/tests.py
+++ ciphers/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Caesar, Atbash, Alphanumeric, Viginere
+
+class CaesarTestCase(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        offset = 3
+        secret = 'VWDQ LV QRW ZKDW KH VHHPV'
+        self.assertIs(future_question.was_published_recently(), False)

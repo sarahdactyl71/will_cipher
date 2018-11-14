8e410057a85355e4b3123df00490008047eba50a
Sarah Kirk <kirkse710@gmail.com>
2018-11-14 14:47:44 -0800

    begins handrolling Caesar create view

--- ciphers/views.py
+++ ciphers/views.py
@@ -16,9 +16,8 @@ def show(request, caesar_id):
     caesar = get_object_or_404(Caesar, pk=caesar_id)
     return render(request, 'ciphers/show.html', {'caesar': caesar})
 
-class AuthorCreate(CreateView):
-    model = Caesar
-    fields = ['caesar_text']
-    
+class create(request):
+    caesar = Caesar.objects.new()
+
 # def destroy(request, cipher_id):
 #     return HttpResponse()

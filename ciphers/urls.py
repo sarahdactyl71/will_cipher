600edb9f3a02f3e09f2eee6aa963d8944911386f
Sarah Kirk <kirkse710@gmail.com>
2018-11-21 14:39:46 -0800

    updates namespacing for ciphers

--- ciphers/urls.py
+++ ciphers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ciphers'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:caesar_id>/', views.show, name='show'),

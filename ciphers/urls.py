85c83133d3b1cf5dc1d0f5b013f0e57bdb095b83
Sarah Kirk <kirkse710@gmail.com>
2018-11-26 10:18:26 -0800

    adds paths in urls.py for update and delete paths

--- ciphers/urls.py
+++ ciphers/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:caesar_id>/', views.show, name='show'),
     path('create/', views.create, name='create'),
+    path('<int:caesar_id>/update/', views.update, name='update'),
+    path('<int:caesar_id>/delete/', views.delete, name='delete'),
 ]

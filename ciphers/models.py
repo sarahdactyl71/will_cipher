8b70ce85179eea8d6c66d5ec45ddd56627729d8f
Sarah Kirk <kirkse710@gmail.com>
2018-11-30 11:13:11 -0800

    removes changes for text decorators

--- ciphers/models.py
+++ ciphers/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.forms import ModelForm
-from django import template
-
-register = template.Library()
 
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
@@ -16,11 +13,9 @@ class Caesar(models.Model):
     def __str__(self):
         return self.caesar_text
 
-    @register.filter
     def encode(self, offset):
         return self.new_message(-offset)
 
-    @register.filter
     def decode(self, offset):
         return self.new_message(offset)
 

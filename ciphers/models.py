f998d9e8f0e70a48c4c1d6ab536b52027d263130
Sarah Kirk <kirkse710@gmail.com>
2018-12-06 15:37:57 -0800

    although list comprehensions would be cool, it iterates far too many times to be helpful.

--- ciphers/models.py
+++ ciphers/models.py
@@ -145,21 +145,20 @@ class Vigenere(models.Model):
         list = []
         new_keyword = self.repeat_keyword(keyword)
         #GRAVITYGRAVITYGRAV
-        for character in self.vigenere_text:
-            #MABELEATSSPRINKLES
-            if character in special_chars:
-                list.append(character)
+        # for keyword_letter, message_letter in zip(new_keyword, self.vigenere_text):
+            #do stuff.
+        for message_char, keyword_char in [(message_char,keyword_char) for message_char in self.vigenere_text for keyword_char in new_keyword]:
+            if message_char not in alphabet:
+                list.append(message_char)
             else:
-                keyword_index = self.vigenere_text.index(character)
-                keyword_letter = new_keyword[keyword_index]
-                import code; code.interact(local=dict(globals(), **locals()))
-                character_index = alphabet.index(character)
+                column_index = alphabet.index(keyword_char)
+                character_index = alphabet.index(message_char)
                 row = alphabet_grid()[character_index]
                 column = row[column_index]
                 list.append(column)
             message = ''.join(list)
-            #SRBZTXYZJSKZBLQCEN
         return message
+            #SRBZTXYZJSKZBLQCEN
 
     # def encode(self, keyword):
     #     new_keyword = self.repeat_keyword(keyword)
